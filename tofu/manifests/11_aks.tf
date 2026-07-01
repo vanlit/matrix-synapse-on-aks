86e731a843d31e2b830f36c399e5f3ffe93a9a36
vanlit <i2.ilin@itransition.com>
2026-07-01 17:59:13 +0200

    remove duping of outputs

--- tofu/manifests/11_aks.tf
+++ tofu/manifests/11_aks.tf
@@ -66,16 +66,3 @@ resource "azurerm_kubernetes_cluster" "main" {
   image_cleaner_interval_hours = 48
   automatic_upgrade_channel = "patch"
 }
-
-output "aks_kube_config_raw" {
-  value     = azurerm_kubernetes_cluster.main.kube_config_raw
-  sensitive = true
-}
-
-output "aks_name" {
-  value = azurerm_kubernetes_cluster.main.name
-}
-
-output "oidc_issuer_url" {
-  value = azurerm_kubernetes_cluster.main.oidc_issuer_url
-}
