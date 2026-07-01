52699434891812467b2f3bce154a194a1b764e1d
vanlit <i2.ilin@itransition.com>
2026-07-01 15:56:46 +0200

    cleanup

--- tofu/install.sh
+++ tofu/install.sh
@@ -1,16 +1,5 @@
 sudo apt install unzip
 
-wget https://github.com/opentofu/opentofu/releases/download/vX.Y.Z/tofu_X.Y.Z_linux_amd64.zip
+wget https://github.com/opentofu/opentofu/releases/download/v1.11.11/tofu_1.11.11_linux_amd64.zip
 unzip tofu_*.zip
 sudo mv tofu /usr/local/bin/
-
-az login
-
-cd infra/tofu
-
-cp terraform.tfvars.example terraform.tfvars
-# edit terraform.tfvars
-
-tofu init
-tofu plan
-tofu apply
